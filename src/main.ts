ac6b474ec72c8720cf0d07ee886b20651cfe892b
michal-cic <michal.cic@gmail.com>
2022-04-02 23:00:49 +0200

    removed comment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-// Added by the CLI
 import "./registerServiceWorker";
 
 import { IonicVue } from "@ionic/vue";
