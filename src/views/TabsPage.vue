5cb40c15751f4e0cc8302c61a2771a8bd9bc1091
michal-cic <michal.cic@gmail.com>
2022-04-02 23:06:54 +0200

    removed deleted tab dependencies

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -21,7 +21,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, triangle } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
@@ -29,7 +29,6 @@ export default defineComponent({
   setup() {
     return {
       ellipse, 
-      square, 
       triangle,
     }
   }
