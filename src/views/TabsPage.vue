f5fcf2173c4751aaf2848ee7fc4b06663235eee1
michal-cic <michal.cic@gmail.com>
2022-04-02 23:05:51 +0200

    removed tab page from nav

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -13,10 +13,6 @@
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
