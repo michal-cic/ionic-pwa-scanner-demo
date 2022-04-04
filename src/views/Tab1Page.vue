d13b0afdadb78e555528bcc73e84e5af2424807b
michal-cic <michal.cic@gmail.com>
2022-04-04 18:30:50 +0200

    added output list

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -15,6 +15,11 @@
         <div class="cam-container">
           <video ref="cameraRef" autoplay></video>
         </div>
+        <div class="results">
+          <div v-for="code in codes" :key="code">
+            {{ code }}
+          </div>
+        </div>
       </div>
     </ion-content>
   </ion-page>
@@ -36,6 +41,8 @@ import {
   DecodeHintType,
 } from "@zxing/library";
 
+const DELAY_MS = 1;
+
 export default defineComponent({
   name: "Tab1Page",
   components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
@@ -43,13 +50,14 @@ export default defineComponent({
     return {
       camera: null as HTMLVideoElement | null,
       reader: null as BrowserMultiFormatReader | null,
+      codes: [] as string[],
     };
   },
   methods: {
     async openToast(message: string) {
       const toast = await toastController.create({
         message,
-        duration: 2000,
+        duration: 1000,
       });
       return toast.present();
     },
@@ -75,18 +83,19 @@ export default defineComponent({
     hints.set(DecodeHintType.POSSIBLE_FORMATS, [
       BarcodeFormat.QR_CODE,
       BarcodeFormat.CODE_39,
+      BarcodeFormat.CODE_128,
     ]);
 
-    this.reader = new BrowserMultiFormatReader();
-
+    this.reader = new BrowserMultiFormatReader(hints, DELAY_MS);
     this.reader.decodeFromVideoDevice(null, this.camera, (result, err) => {
       if (result) {
         console.log(result);
         this.openToast(result.toString());
+        this.codes.push(result.toString());
       }
 
-      if (err && err.getKind() !== 'NotFoundException') {
-        console.error(err.getKind())
+      if (err && err.getKind() !== "NotFoundException") {
+        console.error(err.getKind());
         this.openToast(err.getKind());
       }
     });
@@ -99,7 +108,16 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+}
+
+.results {
+  margin-top: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 640px;
+  height: 640px;
+  overflow-y: scroll;
 }
 
 .cam-container {
